3288ec6b9fd2326c9e40a29fbf87c00bb15a0783
boyangMAE <by57@cornell.edu>
2018-07-17 02:14:34 -0400

    Update draft.R

--- src/draft.R
+++ src/draft.R
@@ -1,10 +1,23 @@
+# read the test data file
 data <- "https://raw.githubusercontent.com/boyangMAE/pharmacy_counting/master/input/inputtest.txt"
+#
+# read the test data file to a dataframe in R
 testdata <- read.table(data,header=TRUE, sep=",")
-
+#
+# count how many precribers for a certain drug
 num_id <- aggregate(data=testdata, id ~ drug_name, function(x) length(unique(x)))
+#                    
+# calculate the total cost of a certain drug
 total_cost <- aggregate(data=testdata, drug_cost ~ drug_name, function(x) sum(x))
+#
+# prepare the output file by merging the two temporary dataframes
 total <- merge(num_id,total_cost)
+#
+# edit the column names
 colnames(total) <- c("drug_name", "num_prescriber","total_cost")
+#
+# sort/order the data by using descending total cost                       
 total <- total[order(-total$total_cost),]
-
+#
+# output file
 write.table(total,file="./outputtest.txt",sep=",",quote=FALSE,row.name=FALSE)
